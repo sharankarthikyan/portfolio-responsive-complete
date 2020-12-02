abf807341e41813827ec934f0b7d7ced8a19aacb
Sharan <karthikeyansharan00@gmail.com>
2020-12-02 09:15:46 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
+// $(window).bind("scroll", function () {
+//   var currentTop = $(window).scrollTop();
+//   var elems = $(".scrollspy");
+//   elems.each(function (index) {
+//     var elemTop = $(this).offset().top;
+//     var elemBottom = elemTop + $(this).height();
+//     if (currentTop >= elemTop && currentTop <= elemBottom) {
+//       var id = $(this).attr("id");
+//       var navElem = $('a[href="#' + id + '"]');
+//       console.log(navElem);
+//       navElem.parent().addClass("active").siblings().removeClass("active");
+//       $(this).addClass("active");
+//       console.log(this);
+//     }
+//   });
+// });
+
 /*===== MENU SHOW =====*/
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
@@ -9,6 +26,7 @@ const showMenu = (toggleId, navId) => {
     });
   }
 };
+
 showMenu("nav-toggle", "nav-menu");
 
 /*===== ACTIVE AND REMOVE MENU =====*/
